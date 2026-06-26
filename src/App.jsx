252114d65c90f6c3f4323b81991cc55a0d5a44c0
raffirupp <raffael.ruppert@sciencespo.fr>
2026-06-26 17:08:24 +0200

    V2 als Startseite, V1/V3 archiviert auf /v1 und /v3

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import V1App from './variants/v1/V1App'
 import V2App from './variants/v2/V2App'
 import V3App from './variants/v3/V3App'
-import VariantSwitcher from './VariantSwitcher'
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<V1App />} />
-        <Route path="/v2" element={<V2App />} />
+        <Route path="/" element={<V2App />} />
+        <Route path="/v1" element={<V1App />} />
         <Route path="/v3" element={<V3App />} />
       </Routes>
-      <VariantSwitcher />
     </HashRouter>
   )
 }
