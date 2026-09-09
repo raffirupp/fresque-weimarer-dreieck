57fad7e63f530fefc4315cc1f40853af02af423d
plaufer <95619188+plaufer@users.noreply.github.com>
2026-09-09 15:43:31 +0200

    Increase logo height in V2Footer component

--- src/variants/v2/V2Footer.jsx
+++ src/variants/v2/V2Footer.jsx
@@ -11,7 +11,7 @@ export default function V2Footer() {
           <img
             src="/images/Logos.svg"
             alt="Freska Weimar"
-            className="h-11 w-auto"
+            className="h-20 w-auto"
           />
 
           <div>
